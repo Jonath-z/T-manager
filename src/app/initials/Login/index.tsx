054e15b8a1cc94529f9b642966383b2eb95fb688
Jonath-z <jonathanzihindula95@gmail.com>
2022-02-23 01:07:37 +0200

    loading page added

--- src/app/initials/Login/index.tsx
+++ src/app/initials/Login/index.tsx
@@ -1,10 +1,22 @@
-import React from "react";
-import LoginPage from "../../modules/components/Login";
+import React from 'react';
+import LoginPage from '../../modules/components/Login';
+import { useAccount } from '../../contexts/task';
+import LoadingPage from '../../modules/components/Login/_modules/Loading';
 
 const Login = () => {
-    return (
+  const { walletConnected, failedToConnect } = useAccount();
+  console.log('is wallet connected', walletConnected);
+  console.log('connection status', failedToConnect);
+
+  return (
+    <>
+      {walletConnected && !failedToConnect ? (
         <LoginPage />
-    );
+      ) : (
+        <LoadingPage />
+      )}
+    </>
+  );
 };
 
 export default Login;

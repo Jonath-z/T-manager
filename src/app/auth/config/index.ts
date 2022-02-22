b0142395570e1feb307f911cccf16498055b93ad
Jonath-z <jonathanzihindula95@gmail.com>
2022-02-22 20:01:46 +0200

    firebase auth with google

---
+++ src/app/auth/config/index.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDXEHJlmH_fymrpBbFAdrltXqQVQFowbbM",
+  authDomain: "tododapp-97125.firebaseapp.com",
+  projectId: "tododapp-97125",
+  storageBucket: "tododapp-97125.appspot.com",
+  messagingSenderId: "373307802054",
+  appId: "1:373307802054:web:eb7f4115591f46202eb2c7"
+};
+
+
+export default firebaseConfig;
---
+++ src/app/auth/index.ts
@@ -0,0 +1,17 @@
+import firebaseConfig from './config';
+import 'firebase/compat/auth';
+import firebase from 'firebase/compat/app';
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: "select_account"
+});
+
+export const signInwithPopup = async () => await auth.signInWithPopup(provider);
+
+
+

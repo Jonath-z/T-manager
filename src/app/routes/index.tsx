412f52676ef0f6fcd559a4a2a78e435492d765a4
Jonath-z <jonathanzihindula95@gmail.com>
2022-02-22 20:02:15 +0200

    loading route added

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,12 +1,22 @@
 import Login from "../initials/Login";
 import { useRoutes } from "react-router-dom";
+import Home from "../initials/Home";
+import Loading from "../initials/Loading";
 
 
 const AppRoutes = () => {
     let routes = useRoutes([
         {
             path: '/',
+            element: <Loading/>
+        },
+        {
+            path: '/auth',
             element: <Login/>
+        },
+        {
+            path: '/home',
+            element: <Home/>
         }
     ]);
 

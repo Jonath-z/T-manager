23d4be6d6b4b96505675d2269a638d181380056d
Jonath-z <jonathanzihindula95@gmail.com>
2022-02-23 16:09:40 +0200

    header

---
+++ src/app/initials/Home/Header/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { RiMenu4Line } from 'react-icons/ri';
+import { useUsers } from '../../../contexts/users';
+
+const Header = () => {
+  return <div>Header</div>;
+};
+
+export default Header;

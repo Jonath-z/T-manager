5ed7f146ffefe8fe54eedc42c6539bd6a1628727
Jonath-z <jonathanzihindula95@gmail.com>
2022-02-23 15:58:24 +0200

    clear

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import AppRoutes from './app/routes';
 import { BrowserRouter as Router } from 'react-router-dom';
 import TasksProvider from './app/contexts/task';
+import UsersProvider from './app/contexts/users';
 
 function App() {
   return (
     <TasksProvider>
-      <Router>
-        <AppRoutes />
-      </Router>
+      <UsersProvider>
+        <Router>
+          <AppRoutes />
+        </Router>
+      </UsersProvider>
     </TasksProvider>
   );
 }

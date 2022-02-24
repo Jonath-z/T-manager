e64824644cee4508e0d7c209222bb3a7786393d7
Jonath-z <jonathanzihindula95@gmail.com>
2022-02-24 21:39:39 +0200

    add task button

---
+++ src/app/modules/components/Home/AddButton/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { HiOutlinePlusSm } from 'react-icons/hi';
+
+interface IProps {
+  onClick: () => void;
+}
+
+const AddButton = ({ onClick }: IProps) => {
+  return (
+    <button
+      className="addButton px-5 py-5 rounded-full absolute bottom-10 right-10 text-white shadow-2xl shadow-black"
+      onClick={onClick}
+    >
+      <HiOutlinePlusSm className="text-2xl" />
+    </button>
+  );
+};
+
+export default AddButton;

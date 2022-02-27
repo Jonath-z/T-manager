b18f716fee17cad8df4279af29c215c9abe4606e
Jonath-z <jonathanzihindula95@gmail.com>
2022-02-27 19:03:46 +0200

    day of the weekreviewed but still a stuck

--- src/app/modules/components/Home/Header/index.tsx
+++ src/app/modules/components/Home/Header/index.tsx
@@ -9,6 +9,7 @@ interface IProps {
 const Header = ({ name, profile }: IProps) => {
   const date = new Date();
   const days = ['Mon', 'Tue', 'wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+  console.log(days[date.getDay()], date.getDay());
   return (
     <div className="py-5 flex justify-around items-center">
       <RiMenu4Line className="float-left  text-3xl text-white" />
@@ -17,7 +18,7 @@ const Header = ({ name, profile }: IProps) => {
           Hi {name}
         </span>
         <span className="font-Mulish font-bold text-white">
-          {days[date.getDay() - 1]} {date.getDate()} {''}
+          {days[date.getDay()]} {date.getDate()} {''}
           {date.getFullYear()}
         </span>
       </p>

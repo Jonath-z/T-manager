7e5fd0933588ae24d23b06f517704472ae998745
Jonath-z <jonathanzihindula95@gmail.com>
2022-02-27 19:07:03 +0200

    @tailwind/formsplugin added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 module.exports = {
-  content: [    "./src/**/*.{js,jsx,ts,tsx}",  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
         Roboto: ['Roboto', 'sans-serif'],
-        Mulish: ['Mulish','sans-serif']
-      }
+        Mulish: ['Mulish', 'sans-serif'],
+      },
     },
   },
-  plugins: [],
-}
+  plugins: [
+    require('@tailwindcss/forms')({
+      strategy: 'class',
+    }),
+  ],
+};

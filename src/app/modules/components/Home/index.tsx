70e4a30d339bdfd5576a04529deb39d1b28c3f5e
Jonath-z <jonathanzihindula95@gmail.com>
2022-02-27 19:04:47 +0200

    useEffect removed, and use context directly

--- src/app/modules/components/Home/index.tsx
+++ src/app/modules/components/Home/index.tsx
@@ -1,8 +1,6 @@
-import React, { useEffect, useState } from 'react';
 import Header from './Header';
 import SearchBar from './Search';
 import AddButton from './AddButton';
-import { useNavigate } from 'react-router-dom';
 import { decrypt } from '../../../utils/helpers/cryptoJS';
 import { useUsers } from '../../../contexts/users';
 import { localStorageGet } from '../../../utils/helpers/localStorage';
@@ -11,63 +9,32 @@ import { useSweepDown } from '../../../contexts/sweep';
 import Tasks from './Task';
 import { useTasks } from '../../../contexts/task';
 
-interface User {
-  name: string;
-  email: string;
-  profile: string;
-}
-
-interface ITasks {
-  id: string;
-  title: string;
-  content: string;
-  remind: boolean;
-  start_time: string;
-  end_time: string;
-  completed: boolean;
-  owner_name: string;
-  owner_email: string;
-}
-[];
-
 const HomePage = () => {
-  const navigate = useNavigate();
-  const [user, setUser] = useState<User>();
-  const [userTasks, setUserTasks] = useState<ITasks[]>([]);
-  const users = useUsers();
+  const { users } = useUsers();
   const tasks = useTasks();
   const { toggleFrame, isFrameOpened } = useSweepDown();
   const token = localStorageGet('to_do_token_');
 
-  console.log(tasks);
+  const email = decrypt(token as string);
 
-  const getUserDataByEmail = (email: string) => {
-    users.forEach((user) => {
-      if (user.email === email) {
-        setUser(user);
-      }
-    });
-    tasks.forEach((task) => {
-      if (task.owner_email === email)
-        setUserTasks((prev) => [...prev, task]);
-    });
-  };
-
-  useEffect(() => {
-    setUserTasks([]);
-    if (token) {
-      const email = decrypt(token);
-      getUserDataByEmail(email);
-    } else {
-      navigate('/');
-    }
-  }, [users]);
+  const userTasks = tasks.filter(
+    (task) => task.owner_email === email,
+  );
+  const usersCollection = users.filter(
+    (user) => user.email === email,
+  );
 
   return (
     <div className="h-full absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-br home">
-      <Header name={user?.name} profile={user?.profile} />
+      <Header
+        name={usersCollection[0]?.name}
+        profile={usersCollection[0]?.profile}
+      />
       <SearchBar />
       <AddButton onClick={toggleFrame} />
+      <p className="text-white text-2xl px-8 font-Mulish font-extrabold py-1">
+        Tasks
+      </p>
       <Tasks tasks={userTasks} name={undefined} />
       {isFrameOpened && <AddTaskFrame />}
     </div>

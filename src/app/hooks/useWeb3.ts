1c545229f60a7d22bbb62330721ab75c4a84b629
Jonath-z <jonathanzihindula95@gmail.com>
2022-03-02 20:38:37 +0200

    useWeb3 hook replace web3 service

---
+++ src/app/hooks/useWeb3.ts
@@ -0,0 +1,75 @@
+import { useEffect, useState } from 'react';
+import Web3 from 'web3';
+import { TO_DO_CONTRACT_ADDRESS } from '../../config';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const ABI = require('../../config');
+
+const useWeb3 = () => {
+  const [account, setAccount] = useState('');
+  const web3_provider = new Web3(
+    Web3.givenProvider || 'http://localhost:8545',
+  );
+
+  const Contract = new web3_provider.eth.Contract(
+    ABI.TO_DO_CONTRACT_ABI,
+    TO_DO_CONTRACT_ADDRESS,
+  );
+
+  useEffect(() => {
+    (async () => {
+      const requestAccount =
+        await web3_provider.eth.requestAccounts();
+      setAccount(requestAccount[0]);
+    })();
+  }, []);
+
+  const createTask = async (
+    title: string | undefined,
+    content: string | undefined,
+    startTime: string | undefined,
+    endTime: string | undefined,
+    ownerName: string | undefined,
+    ownerEmail: string | undefined,
+    date: string | undefined,
+    progress: string,
+  ) => {
+    await Contract.methods
+      .createTasks(
+        title,
+        content,
+        startTime,
+        endTime,
+        ownerName,
+        ownerEmail,
+        date,
+        progress,
+      )
+      .send({ from: account });
+  };
+
+  const createUser = async (
+    name: string | null | undefined,
+    email: string | null | undefined,
+    profile: string | null | undefined,
+  ) => {
+    await Contract.methods
+      .createUser(name, email, profile)
+      .send({ from: account });
+  };
+
+  const updateTaskStatus = async (taskID: number) => {
+    await Contract.methods
+      .updateTaskStatus(taskID)
+      .send({ from: account });
+  };
+
+  return {
+    createTask,
+    createUser,
+    web3_provider,
+    Contract,
+    updateTaskStatus,
+  };
+};
+
+export default useWeb3;

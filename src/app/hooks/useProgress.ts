04af30ff3eaa9c60d3f219c1d215138e16d05013
Jonath-z <jonathanzihindula95@gmail.com>
2022-03-02 20:33:57 +0200

    bug still not fixed: user progress pourcentage does not refresh

--- src/app/hooks/useProgress.ts
+++ src/app/hooks/useProgress.ts
@@ -1,5 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ITasks } from '../types';
+import { localStorageGet } from '../utils/helpers/localStorage';
+import { decrypt } from '../utils/helpers/cryptoJS';
 
 interface ISortedByDate {
   date: string;
@@ -7,47 +9,60 @@ interface ISortedByDate {
   progress: number;
 }
 
-const useProgress = (userTasks: ITasks[]) => {
+const useProgress = (tasks: ITasks[]) => {
   const dateMemo = useState<string>();
   const [sortedByDate] = useState<ISortedByDate[]>([]);
+  const [userTasks, setUserTasks] = useState<ITasks[]>([]);
+
+  const token = localStorageGet('to_do_token_');
+  const email = decrypt(token as string);
+
+  useEffect(() => {
+    setUserTasks(tasks.filter((task) => task.owner_email === email));
+  }, [tasks]);
 
   userTasks.forEach(({ date }) => {
     if (dateMemo.indexOf(date) === -1) dateMemo.push(date);
   });
 
-  dateMemo.forEach((memorizedDate) => {
-    const DalyTask = [];
-    for (let i = 0; i <= userTasks.length; i++) {
-      if (userTasks[i] !== undefined) {
-        if (userTasks[i].date === memorizedDate) {
-          DalyTask.push(userTasks[i]);
-          sortedByDate.push({
-            date: memorizedDate as string,
-            task: DalyTask,
-            progress: 0,
-          });
-        }
+  dateMemo.map((memorizedDate) => {
+    const DalyTask: ITasks[] = [];
+    userTasks.map((task) => {
+      if (task.date === memorizedDate) {
+        DalyTask.push(task);
+        return sortedByDate.push({
+          date: memorizedDate as string,
+          task: DalyTask,
+          progress: 0,
+        });
       }
-    }
+    });
+    return 0;
   });
 
-  const sortedByDateRemovedDuplicated = sortedByDate.filter(
+  const progressArchive = sortedByDate.filter(
     (data, index, self) =>
       index ===
       self.findIndex((element) => element.date === data.date),
   );
 
-  sortedByDateRemovedDuplicated.forEach((task) => {
-    task.task.forEach((currentTask, index) => {
-      if (currentTask.completed) {
-        const progress = (index * 100) / task.task.length;
-        task.progress = progress;
-      }
+  const updateProgress = () => {
+    progressArchive.forEach((task) => {
+      const completedTask = task.task.filter(
+        (task) => task.completed,
+      );
+      const progress =
+        (completedTask.length * 100) / task.task.length;
+      task.progress = Math.floor(progress);
     });
-  });
+  };
 
-  // console.log(sortedByDateRemovedDuplicated);
-  return sortedByDateRemovedDuplicated;
+  updateProgress();
+
+  return {
+    progressArchive,
+    updateProgress,
+  };
 };
 
 export default useProgress;

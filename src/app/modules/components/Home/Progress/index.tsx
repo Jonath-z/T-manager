a94124123f4188bac47480567bf7dd0d9099b775
Jonath-z <jonathanzihindula95@gmail.com>
2022-03-04 20:36:23 +0200

    progress pourcentage justify-between

--- src/app/modules/components/Home/Progress/index.tsx
+++ src/app/modules/components/Home/Progress/index.tsx
@@ -1,26 +1,13 @@
-import React, { useEffect, useState, useMemo } from 'react';
-// import useProgress from '../../../../hooks/useProgress';
-import { useTasks } from '../../../../contexts/task';
-import { BiRefresh } from 'react-icons/bi';
-import { ITasks } from '../../../../types';
-import { localStorageGet } from '../../../../utils/helpers/localStorage';
-import { decrypt } from '../../../../utils/helpers/cryptoJS';
 import { useProgress } from '../../../../contexts/progress';
 
 const Progress = () => {
-  const [isRefreshing, setIsRefreshing] = useState(false);
-
   const progress = useProgress();
-  console.log(progress);
+  // console.log(progress);
 
   return (
     <div>
       <p className="text-white text-2xl px-8 font-Mulish font-extrabold py-1 flex justify-between items-center">
         Progress
-        <BiRefresh
-          className={isRefreshing ? 'animate-spin' : 'animate-none'}
-          // onClick={refresh}
-        />
       </p>
       <div className="flex flex-row progressContainer m-auto py-5">
         {progress
@@ -35,7 +22,9 @@ const Progress = () => {
                   {day.task.length} task
                   {day.task.length === 1 ? '' : 's'}
                 </p>
-                <p>Progress {day.progress}%</p>
+                <p className="flex justify-between items-center">
+                  Progress <span>{day.progress}%</span>
+                </p>
                 <div className="w-full bg-white h-1 my-1 rounded-md">
                   <div
                     className="h-1 bg-amber-600 rounded-md"

c51a1aa73aff067221176e0036b4f4fc1d507e28
Jonath-z <jonathanzihindula95@gmail.com>
2022-03-04 20:38:58 +0200

    click out side hook

---
+++ src/app/hooks/useClickOutside.ts
@@ -0,0 +1,38 @@
+import { useCallback, useEffect, useRef } from 'react';
+
+const useClickOutside = (callback: () => void): any => {
+  const currentRef = useRef<HTMLDivElement>(null);
+
+  const checkParent = (
+    t: { parentNode: any },
+    elm: HTMLDivElement | null,
+  ) => {
+    if (t === elm) {
+      return true;
+    }
+    return false;
+  };
+
+  const check = useCallback(
+    (e) => {
+      const target = (e && e.target) || (e && e.srcElement);
+
+      if (checkParent(target, currentRef.current)) {
+        callback();
+      }
+    },
+    [callback],
+  );
+
+  useEffect(() => {
+    document.addEventListener('click', check, true);
+
+    return () => {
+      document.removeEventListener('click', check, true);
+    };
+  }, [check]);
+
+  return currentRef;
+};
+
+export default useClickOutside;

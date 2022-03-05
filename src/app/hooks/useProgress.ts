9c399efb31e97d67a6ba8ea952afcdeed44048ca
Jonath-z <jonathanzihindula95@gmail.com>
2022-03-05 19:46:29 +0200

    useMemo removed in this component

---
+++ src/app/hooks/useProgress.ts
@@ -0,0 +1,48 @@
+import { ITasks } from '../types';
+
+const useProgress = (tasks: ITasks[], userTasks: ITasks[]) => {
+  console.log(tasks);
+  const dateMemo: string[] = [];
+  const sortedByDate: {
+    date: string;
+    task: ITasks[];
+    progress: number;
+  }[] = [];
+
+  userTasks.forEach(({ date }) => {
+    if (dateMemo.indexOf(date) === -1) dateMemo.push(date);
+  });
+
+  dateMemo.map((memorizedDate) => {
+    const DalyTask: ITasks[] = [];
+    userTasks.map((task) => {
+      if (task.date === memorizedDate) {
+        DalyTask.push(task);
+        return sortedByDate.push({
+          date: memorizedDate as string,
+          task: DalyTask,
+          progress: 0,
+        });
+      }
+    });
+  });
+
+  ////////////////// remove diplicated data ////////////////////
+  const sortedByDateDiplicateRemoved = sortedByDate.filter(
+    (data, index, self) =>
+      index ===
+      self.findIndex((element) => element.date === data.date),
+  );
+  console.log('sorted by date', sortedByDateDiplicateRemoved);
+
+  ///////////////// update progress //////////////////
+  sortedByDateDiplicateRemoved.forEach((task) => {
+    const completedTask = task.task.filter((task) => task.completed);
+    const progress = (completedTask.length * 100) / task.task.length;
+    task.progress = Math.floor(progress);
+  });
+  console.log('sorted by date', sortedByDateDiplicateRemoved);
+  return sortedByDateDiplicateRemoved;
+};
+
+export default useProgress;

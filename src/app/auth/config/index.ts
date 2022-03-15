db041b608c7e4cd430fa8b46a1e6a8336d4f140a
Jonath-z <jonathanzihindula95@gmail.com>
2022-03-15 16:41:25 +0200

    all credentials made private

--- src/app/auth/config/index.ts
+++ src/app/auth/config/index.ts
@@ -1,10 +1,11 @@
 const firebaseConfig = {
-  apiKey: 'AIzaSyDXEHJlmH_fymrpBbFAdrltXqQVQFowbbM',
-  authDomain: 'tododapp-97125.firebaseapp.com',
-  projectId: 'tododapp-97125',
-  storageBucket: 'tododapp-97125.appspot.com',
-  messagingSenderId: '373307802054',
-  appId: '1:373307802054:web:eb7f4115591f46202eb2c7',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:
+    process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export default firebaseConfig;
